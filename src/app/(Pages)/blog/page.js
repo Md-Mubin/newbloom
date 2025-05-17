de732093b5e16139b61ee7985a37ed7d77be2d51
Md Mubin <fontdevmubin@gmail.com>
2025-05-17 12:59:19 +0600

    modified:   src/app/(Pages)/blog/page.js

--- src/app/(Pages)/blog/page.js
+++ src/app/(Pages)/blog/page.js
@@ -1,8 +1,11 @@
+import CommonPagesBanne from '@/app/Commons/CommonPagesBanne'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>blog</div>
+    <>
+      <CommonPagesBanne pageName={"Blog"} pageNameBig={"Blog"} />
+    </>
   )
 }
 
